46bd9472f388f4173a8523a12cc583aa3d4664b7
Seba <goginaalinadmtr@gmail.com>
2025-08-18 03:48:13 +0300

    feat: complete ads rotator task

---
+++ dom/ads/task.js
@@ -0,0 +1,12 @@
+const rotators = document.querySelectorAll('.rotator');
+
+rotators.forEach(rotator => {
+    const cases = rotator.querySelectorAll('.rotator__case');
+    let currentIndex = 0;
+
+    setInterval(() => {
+        cases[currentIndex].classList.remove('rotator__case_active');
+        currentIndex = (currentIndex + 1) % cases.length;
+        cases[currentIndex].classList.add('rotator__case_active');
+    }, 1000);
+});
